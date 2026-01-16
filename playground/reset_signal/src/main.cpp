dec78c517385cc79cda4ba8d411d89ea617e986a
TetsuroAndo <tetsuro997@gmail.com>
2026-01-16 21:39:16 +0900

    refactor: リセット理由の出力を簡素化し、冗長なコードを削除

--- playground/reset_signal/src/main.cpp
+++ playground/reset_signal/src/main.cpp
@@ -1,7 +1,6 @@
-#include "esp_system.h" // esp_reset_reason()
+#include "esp_system.h"
 #include <Arduino.h>
 
-// DeepSleep以外のリセットでも保持される(多くのケースで) RTC領域
 RTC_DATA_ATTR uint32_t bootCount = 0;
 
 static const char *resetReasonToStr(esp_reset_reason_t r) {
@@ -9,25 +8,13 @@ static const char *resetReasonToStr(esp_reset_reason_t r) {
 	case ESP_RST_POWERON:
 		return "POWERON";
 	case ESP_RST_EXT:
-		return "EXT (external reset)";
+		return "EXT";
 	case ESP_RST_SW:
-		return "SW (software reset)";
-	case ESP_RST_PANIC:
-		return "PANIC";
-	case ESP_RST_INT_WDT:
-		return "INT_WDT";
-	case ESP_RST_TASK_WDT:
-		return "TASK_WDT";
-	case ESP_RST_WDT:
-		return "WDT";
-	case ESP_RST_DEEPSLEEP:
-		return "DEEPSLEEP";
+		return "SW";
 	case ESP_RST_BROWNOUT:
 		return "BROWNOUT";
-	case ESP_RST_SDIO:
-		return "SDIO";
 	default:
-		return "UNKNOWN";
+		return "OTHER";
 	}
 }
 
@@ -36,22 +23,16 @@ void setup() {
 	delay(200);
 
 	bootCount++;
-
-	esp_reset_reason_t reason = esp_reset_reason();
+	auto reason = esp_reset_reason();
 
 	Serial.println();
-	Serial.println("=== ESP32 Playground Boot ===");
-	Serial.printf("Boot count: %lu\n", (unsigned long)bootCount);
-	Serial.printf("Reset reason: %s (%d)\n", resetReasonToStr(reason),
+	Serial.println("*** PLAYGROUND RESET-SIGNAL FW BOOTED ***");
+	Serial.printf("bootCount=%lu\n", (unsigned long)bootCount);
+	Serial.printf("resetReason=%s (%d)\n", resetReasonToStr(reason),
 				  (int)reason);
-	Serial.println("Waiting... (Toggle RPi reset to see this again)");
 }
 
 void loop() {
-	// 生きていることが分かるようにハートビート
-	static uint32_t last = 0;
-	if (millis() - last > 1000) {
-		last = millis();
-		Serial.printf("[alive] uptime=%lu ms\n", (unsigned long)millis());
-	}
+	delay(1000);
+	Serial.println("[alive]");
 }
