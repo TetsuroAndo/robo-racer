22fe4689deea6e7aec266c836cccb4f3be957a37
haitooo <haru869pc@gmail.com>
2026-01-17 20:38:49 +0900

    4

--- rpi/main.cpp
+++ rpi/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
     signal(SIGTERM, on_sig);
 
     const char* lidar_dev = (argc >= 2) ? argv[1] : "/dev/ttyAMA2";
-    int         lidar_baud = (argc >= 3) ? std::atoi(argv[2]) : 256000;
+    int         lidar_baud = (argc >= 3) ? std::atoi(argv[2]) : 460800;
     const char* esp_dev   = (argc >= 4) ? argv[3] : "/dev/ttyAMA0";
 
     return run_lidar_to_esp(lidar_dev, lidar_baud, esp_dev);
